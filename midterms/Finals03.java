42cb3c5ce1c6129a0909ddcdf483689364012d1a
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-06-05 12:33:11 +0900

    Create Finals03.java

---
+++ midterms/Finals03.java
@@ -0,0 +1,46 @@
+class X {
+    int val;
+    X(int val) {
+        this.val = val;
+    }
+    void foo() {
+        System.out.println(val);
+    }
+}
+
+class Y extends X{
+    Y(int val) {
+        super(val);
+    }
+    void foo() {
+        System.out.println(val-1);
+    }
+}
+
+
+class Z extends X{
+    Z(int val) {
+        super(val);
+    }
+    void foo() {
+        System.out.println(val+1);
+    }
+}
+
+
+public class Finals03 {
+    public static void main(String[] args) {
+        int data[] = {2,7,6,3,1};
+        X x[] = new X[5];
+
+        for(int i=0; i<5; i++) {
+            if(data[i]/2*2 == data[i]) {
+                x[i] = new Y(data[i]);
+            } else {
+                x[i] = new Z(data[i]);
+            }
+            x[i].foo();
+        }
+    }
+    
+}
