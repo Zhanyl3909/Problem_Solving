0b79087bf68871f7688d47a88c58184eb48ce342
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-06-15 15:46:56 +0900

    Update Finals08.java

--- midterms/Finals08.java
+++ midterms/Finals08.java
@@ -1,1 +1,13 @@
 
+class Finals08 {
+   public static void main(String args[])
+   {
+      int x = 31;
+      for (int i = 0; i < 4; i++)
+		   x = x >> 1;
+	   x = x | 8;
+      x = x >> 2;
+      x = x & 15;
+      System.out.println("x = "+x);
+   }
+}
