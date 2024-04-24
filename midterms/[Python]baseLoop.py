d85e14a8441b8cae7a1e2407b91a12f610808d10
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2024-04-24 17:36:44 +0900

    Create [Python]baseLoop.py

---
+++ midterms/[Python]baseLoop.py
@@ -0,0 +1,39 @@
+for i in range(5):
+    print(i)
+
+
+# Output 
+"""
+0
+1
+2
+3
+4
+5
+"""
+
+
+for i in range(5, 10):
+    print(i)
+
+# Output 
+"""
+5
+6
+7
+8
+9까지만 
+"""
+
+for i in range(5, 15, 3):
+    print(i)
+
+# Output 
+# for i in range(x, y, step)
+"""
+5
+8
+11
+14
+"""
+
